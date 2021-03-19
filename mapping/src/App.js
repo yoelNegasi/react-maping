4e8b61c6ad8ef5edf61694a8e2277deac7487d5e
yoelng <yoelgoitom25@gmail.com>
2021-03-19 16:09:34 -0700

    map

--- mapping/src/App.js
+++ mapping/src/App.js
@@ -1,13 +1,54 @@
+import axios from "axios";
+import React, { Component } from "react";
 import "./App.css";
-import Axios from "./Axios";
 
-function App() {
-  return (
-    <div>
-      <h1>hello</h1>
-      <Axios />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: [],
+    };
+    this.initialize = this.initialize.bind(this);
+  }
+  initialize() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        this.setState({ users: response.data });
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  }
+  componentDidMount() {
+    this.initialize();
+  }
+  render() {
+    console.log("state", this.state.users);
+    let elements = this.state.users.map((el) => {
+      return (
+        <tr>
+          <td>{el.id}</td>
+          <td>{el.userId}</td>
+          <td>{el.title}</td>
+        </tr>
+      );
+    });
+    return (
+      <div>
+        <table>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>body</th>
+          </tr>
+          <tbody>{elements}</tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
